e8e35ba80bf17da543ad41673ab26c3f155a3ff2
ayaka_mukai <ayaka_mukai@bwave.co.jp>
2020-08-05 02:30:14 +0900

    [add]CalendarControllerに処理を追記

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -7,6 +7,7 @@ use Exception;
 use App\Http\Requests\CalendarRequest;
 use Illuminate\Support\Facades\Storage;
 use Carbon\Carbon;
+use App\Schedule;
 
 class CalendarController extends Controller
 {
@@ -24,6 +25,10 @@ class CalendarController extends Controller
         }else{
             $month = Carbon::now()->month;
         }
+
+        $start = $year.'-'.$month.'-1';
+        $end = $year.'-'.$month.'-31';
+        $plan_date_dbs = Schedule::whereBetween('date', [$start, $end])->get();        
 
         $weeks = ['日','月','火','水','木','金','土'];
         $carbon = Carbon::create($year, $month, 1, 0,0,0);
@@ -57,6 +62,83 @@ class CalendarController extends Controller
         }
 
         return view('calendars.calendar', ['year' => $year, 'month' => $month, 'last_month' => $last_month, 'last_month_year' => $last_month_year, 'next_month' => $next_month, 'next_month_year' => $next_month_year,
-        'weeks' => $weeks, 'first_weekday' => $first_weekday, 'final_weekday' => $final_weekday, 'holidays' => $holidays, 'final_day' => $final_day, 'today' => $today]);
+        'weeks' => $weeks, 'first_weekday' => $first_weekday, 'final_weekday' => $final_weekday, 'holidays' => $holidays, 'final_day' => $final_day, 'today' => $today, 'plan_date_dbs' => $plan_date_dbs]);
     }
+
+    //スケジュール画面
+    public function show(CalendarRequest $request)
+    {
+
+        $date_db = str_replace("/", "-", $request->query('schedule'));
+        $date_array = explode('-', $date_db);
+
+        var_dump($date_array);
+        $results = Schedule::where('date', $date_db)->orderBy('time','asc')->get();
+        return view('calendars.schedule', ['results' => $results, 'date_array' => $date_array, 'date_db' => $date_db]);
+    
+    }
+
+    //登録
+    public function create(CalendarRequest $request)
+    {
+
+        $date = $request->query('schedule');
+        $date_array = explode('-', $date);
+        var_dump($date_array);
+
+        return view('calendars.create', ['date_array' => $date_array, 'date' => $date]);
+    
+    }
+
+    //登録処理
+    public function store(CalendarRequest $request)
+    {
+
+        // DB登録
+        $results = new Schedule;
+        $results->title = $request->title;
+        $results->plan = $request->plan;
+        $results->time = $request->hour.":".$request->minute;
+        $results->date = $request->date;
+        $results->save();
+
+        return redirect()->route('calendar');
+    }
+
+    //編集
+    public function edit($id)
+    {
+        $results = Schedule::find($id);
+        var_dump($results->date);
+
+        return view('calendars.edit', ['results' => $results]);
+    }
+
+    //更新処理
+    public function update(CalendarRequest $request, $id)
+    {
+        // DBに更新
+        //$results = User::find($id);
+        //$results->fill($request->all());
+        //$results->save();
+
+        $results = Schedule::find($id);
+        $results->title = $request->title;
+        $results->plan = $request->plan;
+        $results->time = $request->time;
+        $results->date = $request->date;
+        $results->save();
+        return redirect()->route('schedule');
+    }
+
+    // ユーザー削除処理
+    public function delete($id)
+    {
+        $results = Schedule::find($id);
+        $results->delete();
+
+        return redirect()->route('schedule');
+    }
+
+
 }
