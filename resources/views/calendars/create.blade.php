29d1d49a9465baf0ece215677d84fc0cff3e6b78
ayaka_mukai <ayaka_mukai@bwave.co.jp>
2020-08-07 02:48:07 +0900

    [fix]登録・編集画面の修正
    ・入力値保持
    ・完了メッセージの表示
    ・バリデーションエラーメッセージの表示

--- resources/views/calendars/create.blade.php
+++ resources/views/calendars/create.blade.php
@@ -2,6 +2,13 @@
 @section('title', 'Schedule登録')
 @section('content')
 
+<style type="text/css">
+  .invalid-feedback{
+    display: block;
+  }
+  
+</style>
+
 <body>
 
   <div class="container my-2 p-3 bg-white">
@@ -10,14 +17,7 @@
         <h3 class="text-center">
           {{ $date_array[0] }}年{{ $date_array[1] }}月{{ $date_array[2] }}日のスケジュール
         </h3>
-<?php
-//未入力エラーメッセージ
-session_start();
-if(!empty($_SESSION['errMsg'])){
-  echo "<div class='alert alert-danger'>".$_SESSION['errMsg']."</div>"; 
-}
-session_destroy();
-?>
+
         <form action="{{ route('store') }}" method="post">
         {{ csrf_field() }}
         <div class="form-group row">
@@ -26,9 +26,9 @@ session_destroy();
               <select class="form-control" name="hour">
                  @for($i=0; $i<=23; $i++)
                   <option value="{{ $i }}"
-                  @if(!empty($_SESSION['input']['hour']) && $_SESSION['input']['hour'] == $i)
-                    selected
-                  @endif
+                    @if(old('hour') == $i)
+                      selected
+                    @endif
                    >{{ $i }}</option>
                  @endfor
               </select>
@@ -41,9 +41,9 @@ session_destroy();
                     $y = str_pad($i, 2, 0, STR_PAD_LEFT)
                   @endphp
                     <option value="{{ $y }}"
-                  @if(!empty($_SESSION['input']['minute']) && $_SESSION['input']['minute'] == $y) 
-                    selected
-                  @endif
+                      @if(old('minute') == $y)
+                        selected
+                      @endif
                   >{{ $y }}</option>
                 @endfor
               </select>
@@ -53,13 +53,23 @@ session_destroy();
           <div class="form-group row">
             <label for="title" class="col-4 col-form-label">タイトル</label>
               <div class="col-6">
-                <input type="text" class="form-control" name="title" value="<?php if(!empty($_SESSION['input']['title'])) {echo htmlspecialchars($_SESSION['input']['title']); } ?>">
+                <input type="text" class="form-control @if($errors->has('title')) is-invalid @endif" name="title" value="{{ old('title') }}">
+                @if ($errors->has('title'))
+                  <div class="invalid-feedback">
+                    {{ $errors->first('title') }}
+                  </div>
+                @endif
               </div>
           </div>
 
           <div class="form-group row">
             <div class="col-9 mx-auto">
-              <textarea class="form-control" name="plan" rows="5"><?php if(!empty($_SESSION['input']['plan'])) {echo htmlspecialchars($_SESSION['input']['plan']); } ?></textarea>
+              <textarea class="form-control @if($errors->has('title')) is-invalid @endif" name="plan" rows="5">{{ old('plan') }}</textarea>
+              @if ($errors->has('plan'))
+                <div class="invalid-feedback">
+                  {{ $errors->first('plan') }}
+                </div>
+              @endif
             </div>
           </div>
 
--- resources/views/calendars/edit.blade.php
+++ resources/views/calendars/edit.blade.php
@@ -7,41 +7,39 @@
   <div class="container my-2 p-3 bg-white">
     <h3>スケジュール編集</h3>
     <div class="text-center">
-        <h3 class="text-center"><?php echo $year."年".$month."月".$day."日のスケジュール" ?></h3>
-<?php
-//未入力エラーメッセージ
-session_start();
-if(!empty($_SESSION['errMsg'])) {
-  echo "<div class='alert alert-danger'>".$_SESSION['errMsg']."</div>";
-}
-session_destroy();
-?>
-        <form action="edit_register.php?schedule=<?= $schedule ?>&id=<?= $id ?>" method="post">
+        <h3 class="text-center">
+          {{ $date_array[0] }}年{{ $date_array[1] }}月{{ $date_array[2] }}日のスケジュール
+        </h3>
 
+        <form action="{{ route('update', $result->id) }}" method="post">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
           <div class="form-group row">
             <label for="time" class="col-4 col-form-label">時間</label>
               <div class="col-2">
                 <select class="form-control" name="hour">
-                  <?php for($i=0; $i<=23; $i++){
-                    echo '<option value="'.$i.'"';
-                    if(ltrim($time_array[0], '0') == $i){ 
-                      echo 'selected';
-                      } 
-                    echo '>'.$i.'</option>';
-                  }?>
+                  @for($i=0; $i<=23; $i++)
+                    <option value={{ $i }}
+                      @if(old('hour', $hour) == $i)
+                        selected
+                      @endif
+                    >{{ $i }}</option>
+                  @endfor
                 </select>
               </div>
               <span>：</span>
               <div class="col-2">
                 <select class="form-control" name="minute">
-                <?php for($i=0; $i<=59; $i++){
-                        $y = str_pad($i, 2, 0, STR_PAD_LEFT);
-                        echo '<option value="'.$y.'"';
-                        if($time_array[1] == $y){ 
-                          echo 'selected';
-                        } 
-                        echo '>'.$y.'</option>';
-                      }?>
+                  @for($i=0; $i<=59; $i++)
+                    @php
+                      $y = str_pad($i, 2, 0, STR_PAD_LEFT);
+                    @endphp
+                      <option value={{ $y }}
+                         @if(old('minute', $minute) == $y)
+                           selected
+                         @endif
+                      >{{ $y }}</option>
+                   @endfor
                 </select>
               </div>  
           </div>
@@ -49,21 +47,23 @@ session_destroy();
           <div class="form-group row">
             <label for="title" class="col-4 col-form-label">タイトル</label>
               <div class="col-6">
-                <input type="text" class="form-control" name="title" value="<?php echo htmlspecialchars($results[0]['title']);?>">
+                <input type="text" class="form-control" name="title" value="{{ old('title', $result->title) }}">
               </div>
           </div>
 
           <div class="form-group row">
             <div class="col-9 mx-auto">
-              <textarea class="form-control" name="plan" rows="5"><?php echo htmlspecialchars($results[0]['plan']);?></textarea>
+              <textarea class="form-control" name="plan" rows="5">{{ old('plan', $result->plan) }}</textarea>
             </div>
           </div>
 
-          <button class="btn btn-primary col-2" type="submit">登録する</button>
+          <input type="hidden" class="form-control" name="date" value="{{ $date_array[0] }}-{{ $date_array[1] }}-{{ $date_array[2] }}">
+          
+          <button class="btn btn-primary col-2" type="submit">更新する</button>
         </form>
 
       <div class="my-5">
-      <a href="schedule.php?schedule=<?= $schedule ?>">戻る</a>
+      <a href="{{ route('schedule', ['schedule' => $date]) }}">戻る</a>
       </div>
 
     </div>
