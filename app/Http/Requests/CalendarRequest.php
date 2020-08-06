88710e737d564f8fa405d900085e7b50ad393945
ayaka_mukai <ayaka_mukai@bwave.co.jp>
2020-08-07 02:50:30 +0900

    [fix]バリデーションルールの追加

--- app/Http/Requests/CalendarRequest.php
+++ app/Http/Requests/CalendarRequest.php
@@ -24,31 +24,35 @@ class CalendarRequest extends FormRequest
     public function rules()
     {
         return [
-            'year'=>'regex:/^[0-9]{4}$/',
-            'month'=>'regex:/^[0-9]{1,2}$/',
-            'date'=>'date'
+            'title' => 'max:20',
+            // 'title' => 'required',
+            'plan' => 'max:100',
+            'year' => 'regex:/^[0-9]{4}$/',
+            'month' => 'regex:/^[0-9]{1,2}$/',
+            'date' => 'date',
         ];
     }
-    public function getValidatorInstance()
+    // public function getValidatorInstance()
+    // {
+    //     if ($this->query('year') && $this->query('month'))
+    //     {
+    //         $dateFormat = [$this->query('year'), $this->query('month'), 1];
+    //         $checkDate = implode("-", $dateFormat);
+    //         $this->merge([
+    //             'date' => $checkDate
+    //         ]);
+    //     }
+
+    //     return parent::getValidatorInstance();
+    // }
+
+    public function messages()
     {
-        if ($this->query('year') && $this->query('month'))
-        {
-            $dateFormat = [$this->query('year'), $this->query('month'), 1];
-            $checkDate = implode("-", $dateFormat);
-            $this->merge([
-                'date' => $checkDate
-            ]);
-        }
-
-        return parent::getValidatorInstance();
-    }
-
-    public function messages() {
         return [
-        "required" => "必須項目です。",
-        "email" => "メールアドレスの形式で入力してください。",
-        "numeric" => "数値で入力してください。",
-        "opinion.max" => "500文字以内で入力してください。"
+        "title.required" => "タイトルを入力してください",
+        "title.max" => "タイトルは20文字以内で入力してください",
+        "plan.max" => "内容は100文字以内で入力してください"
+
         ];
       }
 }
