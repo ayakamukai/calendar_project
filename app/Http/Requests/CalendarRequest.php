2f4a6032f1dd2ce970f974ba4cc445f547b6534b
ayaka_mukai <ayaka_mukai@bwave.co.jp>
2020-08-17 02:38:07 +0900

    [fix]時間のバリデージョンを追加

--- app/Http/Requests/CalendarRequest.php
+++ app/Http/Requests/CalendarRequest.php
@@ -25,9 +25,10 @@ class CalendarRequest extends FormRequest
     {
         return [
             'title' => 'max:20|required',
-            'plan' => 'max:30|required',
-            'time' => 'date_format:G:i'
-        ];
+            'plan' => 'max:100',
+            'time' => ['date_format:G:i',
+                'regex/^[0-9]{1,2}:[0-9]{2}$/']
+            ];
     }
 
     public function messages()
@@ -35,8 +36,9 @@ class CalendarRequest extends FormRequest
         return [
         "title.required" => "タイトルを入力してください",
         "title.max" => "タイトルは20文字以内で入力してください",
-        "plan.required" => "内容を入力してください",
-        "plan.max" => "内容は30文字以内で入力してください"
+        "plan.max" => "内容は100文字以内で入力してください",
+        "time.date_format" => "HH:SS形式で入力してください",
+        "time.regex" => "正しい時刻を入力してください",
         ];
       }
 }
